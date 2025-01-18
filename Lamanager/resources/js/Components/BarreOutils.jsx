dcf2bee5a8cd529a1846f7287007c048c3c578ca
Matoucoucou <matysdeschamps87@gmail.com>
2025-01-18 18:22:52 +0100

    popup indicattion des boutons de la barre d'outils

--- Lamanager/resources/js/Components/BarreOutils.jsx
+++ Lamanager/resources/js/Components/BarreOutils.jsx
@@ -10,6 +10,7 @@ function BarreOutils({ toggleIcons }) {
     const [showCustomPopup, setShowCustomPopup] = useState(false);
     const [showRolePopup, setShowRolePopup] = useState(false);
     const [promoName, setPromoName] = useState('');
+    const [popupInfoButton, setPopupInfoButton] = useState("")
     const params = new URLSearchParams(window.location.search);
     const promoId = params.get('promo_id');
 
@@ -30,16 +31,52 @@ function BarreOutils({ toggleIcons }) {
 
         fetchPromoName();
     }, [promoId]);
+    function handleMouseEnter(e,text) {
+        const cell = e.target; // Récupère la cellule cible
+        const rect = cell.getBoundingClientRect();
+        setPopupInfoButton({
+            visible: true,
+            text : text,
+            x : rect.x,
+            y : rect.y,
+            height : rect.height
+        })
+    }
+    function handleMouseLeave() {
+        setPopupInfoButton({ visible: false, x: 0, y: 0, content: '' });
+    }
 
     return (
+
         <>
+            {popupInfoButton.visible && (
+                <div
+                    style={{
+                        position: 'fixed',
+                        top: popupInfoButton.y + popupInfoButton.height,
+                        left: popupInfoButton.x - 50,
+                        backgroundColor: 'white',
+                        border: '1px solid black',
+                        paddingRight : '5px',
+                        paddingLeft : '5px',
+                        borderRadius: '5px',
+                        zIndex: 1000,
+                        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+                        display: 'flex',
+
+
+                    }}
+                >
+                    {popupInfoButton.text}
+                </div>
+            )}
             <Link href="/">
                 <div className="header-content">
                     <img src={logo} alt="Logo" className="logo" />
                     <h1 className="title">Lamanager</h1>
                 </div>
             </Link>
-    
+
             {window.location.pathname === '/tableau' && (
                 <div className="NomPromo">
                     <h2>{promoName}</h2>
@@ -48,17 +85,34 @@ function BarreOutils({ toggleIcons }) {
 
             <div className="BarreOutils">
                 <ul className="barre-outils-list">
-                    <li className="barre-outils-item" onClick={() => setShowCustomPopup(true)}><Table/></li>
-                    <li className="barre-outils-item"><Link href="/versionProf"><ChartColumnIncreasing /></Link></li>
-                    <li className="barre-outils-item" onClick={toggleIcons}><MousePointer2 /></li>
-                    <li className="barre-outils-item"><Settings /></li>
-                    <li className="barre-outils-item">
+                    <li className="barre-outils-item" onClick={() => setShowCustomPopup(true)}
+
+                        onMouseEnter={(e) => handleMouseEnter(e,"Importation du MCCC")}
+                        onMouseLeave={handleMouseLeave}
+
+                    ><Table/></li>
+
+                    <li className="barre-outils-item"
+                        onMouseEnter={(e) => handleMouseEnter(e,"Version Professeur")}
+                        onMouseLeave={handleMouseLeave}>
+                        <Link href="/versionProf"><ChartColumnIncreasing /></Link></li>
+
+                    <li className="barre-outils-item" onClick={toggleIcons}
+                        onMouseEnter={(e) => handleMouseEnter(e,"Outil de sélection")}
+                        onMouseLeave={handleMouseLeave}><MousePointer2 /></li>
+                    <li className="barre-outils-item"
+                        onMouseEnter={(e) => handleMouseEnter(e,"Gestion des comptes")}
+                        onMouseLeave={handleMouseLeave}>
                         <Link href={"/profil?from=tableau"}>
                             <BookUser />
                         </Link>
                     </li>
-                    <li className="barre-outils-item" onClick={() => setShowRolePopup(true)}><FileUser /></li>
-                    <li className="barre-outils-item"><Download /></li>
+                    <li className="barre-outils-item" onClick={() => setShowRolePopup(true)}
+                        onMouseEnter={(e) => handleMouseEnter(e,"Gestion des roles")}
+                        onMouseLeave={handleMouseLeave}><FileUser /></li>
+                    <li className="barre-outils-item"
+                        onMouseEnter={(e) => handleMouseEnter(e,"Téléchargement du prévisionel")}
+                        onMouseLeave={handleMouseLeave}><Download /></li>
                 </ul>
             </div>
             {showCustomPopup && (
