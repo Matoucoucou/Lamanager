341fe4ec7adf8226d38f4e0b531f1cebc78b120b
KadxxV <mael.court@etu.unilim.fr>
2025-01-30 10:13:05 +0100

    responsive profil

--- Lamanager/resources/js/Components/ProfilLeftPart.jsx
+++ Lamanager/resources/js/Components/ProfilLeftPart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { User, LogOut } from 'lucide-react';
-import { Link, useForm } from '@inertiajs/react';
+import { useForm } from '@inertiajs/react';
 
 export default function ProfilLeftPart({ userName = 'Utilisateur' }) {
     const { post } = useForm();
@@ -15,15 +15,15 @@ export default function ProfilLeftPart({ userName = 'Utilisateur' }) {
 
     return (
         <div className="flex flex-col items-center p-4">
-            <div className="w-32 h-32 bg-purple-700 rounded-full flex items-center justify-center mb-4">
+            <div className="h-3/4 bg-purple-700 rounded-full flex items-center justify-center mb-4">
                 <User className="w-20 h-20 text-white" />
             </div>
-            <h2 className="text-xl font-semibold mb-6">{userName}</h2>
-            <button
-                className="flex items-center gap-2 text-gray-600 hover:text-gray-800"
+                <h2 className="text-[clamp(1rem, 2.5vw, 2rem)] font-semibold mb-6 text-center">{userName}</h2>
+                <button
+                className="flex items-center gap-2 text-gray-600 hover:text-gray-800 text-sm"
                 onClick={handleLogout}
-            >
-                <LogOut className="w-6 h-6" />
+                >
+                <LogOut className="w-5/6" />
                 Déconnexion
             </button>
         </div>
